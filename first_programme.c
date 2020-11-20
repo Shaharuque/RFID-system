c342c5b0291d89ff0ffdac5790d2bad82c302360
devshaik444 <68109722+devshaik444@users.noreply.github.com>
2020-11-20 22:24:17 +0600

    mainprogramme

--- first_programme.c
+++ first_programme.c
@@ -15,3 +15,18 @@ int mul(int num1, int num2) {
    num3 = num1 * num2;
    return (num3);
 }
+
+int main() {
+   int num1, num2, res;
+ 
+   printf("\nEnter the two numbers : ");
+   scanf("%d %d", &num1, &num2);
+ 
+   //Call Function Sum With Two Parameters
+   res = sum(num1, num2);
+   res1 = sub(num1,num2):
+ 
+   printf("nAddition of two number is : ",res);
+   printf("nsubtraction of two number is : ",res1);
+   return (0);
+}
