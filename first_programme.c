dbe0dea3980e13ec4977763987ed2bffdb001a0b
devshaik444 <68109722+devshaik444@users.noreply.github.com>
2020-11-20 22:48:54 +0600

    ugraded

--- first_programme.c
+++ first_programme.c
@@ -24,9 +24,12 @@ int main() {
  
    //Call Function Sum With Two Parameters
    res = sum(num1, num2);
-   res1 = sub(num1,num2):
+   res1 = sub(num1,num2);
+   res2=div(num1,num2);
+   
  
    printf("nAddition of two number is : ",res);
    printf("nsubtraction of two number is : ",res1);
+   printf("ndivision of two number is : ",res2);
    return (0);
 }
